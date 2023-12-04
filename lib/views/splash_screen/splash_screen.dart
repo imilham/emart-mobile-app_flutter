77cb97dd5b67f3a05d9571af866a3394cdc5fade
imilham <im.ilhamk@gmail.com>
2023-12-04 12:24:44 +0530

    Initial commit

---
+++ lib/views/splash_screen/splash_screen.dart
@@ -0,0 +1,31 @@
+import 'package:emart_app/consts/consts.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: redColor,
+      body: Center(
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Image.asset(
+                icSplashBg,
+                width: 300,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
